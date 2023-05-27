3a66ae5804daf614ea15f75e7fd167c8c2f7e73e
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-05-27 17:32:43 +0430

    VillaOwner view

--- Villa-Sara/villaowner/views.py
+++ Villa-Sara/villaowner/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.filters import SearchFilter
+# from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import VillaOwner
+from .serializers import VillaOwnerSerializer
+
+
+class BusinessOwnerViewSet(ModelViewSet):
+    queryset = VillaOwner.objects.all()
+    serializer_class = VillaOwnerSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['phone_number']
+    # permission_classes = [IsAuthenticated]
+    ordering_fields = '__all__'
