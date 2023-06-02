6f7eb785dd61bfa7d0c572886ac7f20b7fa9e740
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-06-02 13:23:57 +0400

    Update villa owner model

--- Villa-Sara/villaowner/models.py
+++ Villa-Sara/villaowner/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-from jdatetime import datetime as jdatetime_datetime
-
 
 class VillaOwner(models.Model):
     first_name = models.CharField(max_length=32, null=False, blank=False, verbose_name="نام")
@@ -13,21 +11,10 @@ class VillaOwner(models.Model):
 
     phone_number = models.CharField(max_length=15, null=False, blank=False, unique=True, verbose_name="تلفن همراه")
 
-    created_at = models.CharField(max_length=31, null=True, blank=True, verbose_name="زمان ثبت")
+    created_at = models.DateTimeField(max_length=31, null=True, blank=True, auto_now = True, verbose_name="زمان ثبت")
+    updated_at = models.DateTimeField(max_length=31, null=True, blank=True, auto_now_add=True,
+                                      verbose_name="زمان آخرین بروزرسانی")
 
     class Meta:
         verbose_name_plural = "مالکان ویلاها"
         verbose_name = "مالک ویلا"
-
-    def __str__(self):
-        if self.first_name:
-            return f"{self.first_name} {self.last_name}"
-        else:
-            return f"{self.phone_number}"
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            now_local = timezone.now()
-            now_jdatetime = jdatetime_datetime.fromgregorian(datetime=now_local)
-            self.created_at = now_jdatetime.strftime('%Y/%m/%d %H:%M:%S')
-        super().save(*args, **kwargs)
