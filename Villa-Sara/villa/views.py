9b3c3772dfcc4da9eadce89bc809da6144e8262d
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-06-07 21:37:19 +0330

    update villa views

--- Villa-Sara/villa/views.py
+++ Villa-Sara/villa/views.py
@@ -1,5 +1,3 @@
-from rest_framework.filters import SearchFilter
-# from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Villa
@@ -9,7 +7,4 @@ from .serializers import VillaSerializer
 class VillaViewSet(ModelViewSet):
     queryset = Villa.objects.all()
     serializer_class = VillaSerializer
-    # filter_backends = [SearchFilter]
-    # search_fields = ['phone_number']
-    # permission_classes = [IsAuthenticated]
     ordering_fields = '__all__'
