1fa27d8e2e3b55676567c41b51705132fe51e8b9
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-06-07 21:26:31 +0330

    add image view

--- Villa-Sara/image/views.py
+++ Villa-Sara/image/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Image
+from .serializers import ImageSerializer
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
