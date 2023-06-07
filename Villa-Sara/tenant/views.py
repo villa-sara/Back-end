4a7e52c2178913f985a5c8238d1e5e81f7c8ead0
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-06-07 21:33:04 +0330

    add tenant views

--- Villa-Sara/tenant/views.py
+++ Villa-Sara/tenant/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Tenant
+from .serializers import TenantSerializer
+
+
+class VillaOwnerViewSet(ModelViewSet):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+    ordering_fields = '__all__'
