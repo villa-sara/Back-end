34cc5f16d967653cefdbdbfda43cf01d747e8066
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-06-07 21:24:03 +0330

    update image model

--- Villa-Sara/image/models.py
+++ Villa-Sara/image/models.py
@@ -1,27 +1,16 @@
 from django.db import models
-from django.utils import timezone
-from jdatetime import datetime as jdatetime_datetime
-
-from villaowner.models import VillaOwner
+from .. villaowner.models import VillaOwner
+from .. villa.models import Villa
 
 
 class Image(models.Model):
     image = models.TextField(blank=True, verbose_name="تصویر")
     villa_owner = models.ForeignKey(VillaOwner, on_delete=models.CASCADE, related_name='identity_certification_image',
                                     verbose_name="تصویر کارت ملی یا شناسنامه")
+    villa = models.ForeignKey(Villa, on_delete=models.CASCADE, related_name='villa_images', verbose_name='ویلا')
 
     created_at = models.CharField(max_length=31, null=True, blank=True, verbose_name="زمان ثبت")
 
     class Meta:
         verbose_name_plural = "تصاویر"
         verbose_name = "تصویر"
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            now_local = timezone.now()
-            now_jdatetime = jdatetime_datetime.fromgregorian(datetime=now_local)
-            self.created_at = now_jdatetime.strftime('%Y/%m/%d %H:%M:%S')
-        super().save(*args, **kwargs)
-
-    # def __str__(self):
-    #     return self.title
