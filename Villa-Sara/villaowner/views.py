792ab0c39c0e14cada2a479ecd335b98597dd6af
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-06-07 21:34:30 +0330

    update villaowner serializers

--- Villa-Sara/villaowner/views.py
+++ Villa-Sara/villaowner/views.py
@@ -1,5 +1,3 @@
-from rest_framework.filters import SearchFilter
-# from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import VillaOwner
@@ -9,7 +7,4 @@ from .serializers import VillaOwnerSerializer
 class BusinessOwnerViewSet(ModelViewSet):
     queryset = VillaOwner.objects.all()
     serializer_class = VillaOwnerSerializer
-    filter_backends = [SearchFilter]
-    search_fields = ['phone_number']
-    # permission_classes = [IsAuthenticated]
     ordering_fields = '__all__'
