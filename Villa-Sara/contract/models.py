e37cd4aeabbfc2b6c09a7f53a742fc8f02c06462
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2023-06-25 14:53:22 +0330

    delete confirmations from contract model

--- Villa-Sara/contract/models.py
+++ Villa-Sara/contract/models.py
@@ -16,8 +16,8 @@ class Contract(models.Model):
     #                                 null=False, blank=False, verbose_name="بازه زمانی")
     total_price = models.DecimalField(max_digits=16, decimal_places=4, verbose_name="قیمت کل")
 
-    is_confirmed_by_landowner = models.BooleanField(default=False)
-    is_confirmed_by_tenant = models.BooleanField(default=False)
+    # is_confirmed_by_landowner = models.BooleanField(default=False)
+    # is_confirmed_by_tenant = models.BooleanField(default=False)
 
     # start_date = models.DateField(null=False, blank=False, verbose_name="تاریخ شروع اجاره")
     # end_date = models.DateField(null=False, blank=False, verbose_name="تاریخ پایان اجاره")
